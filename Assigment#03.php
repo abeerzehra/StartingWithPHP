7a04777ecb1a23cdf7446f8c5a2d608ec0e9f411
abeerzehra <k164068@nu.edu.pk>
2020-09-06 20:37:23 +0500

    temp updated

--- Assigment#03.php
+++ Assigment#03.php
@@ -29,11 +29,14 @@
     $array[]= array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
 
     // print_r($array);
-
+    $sum =0;
     //Avg temprature
     for($i=0 ; $i < sizeof($array) ; $i++){
-        
+        $sum = $sum + $array{$i};
     }
+    $avg = $sum / sizeof($array);
+
+    echo "Average temprature :-".$avg;
  
 
 ?>
